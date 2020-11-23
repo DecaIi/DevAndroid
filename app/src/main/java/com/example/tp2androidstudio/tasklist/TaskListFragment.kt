66da70f5adaef80038a43fb9d74b288f843fce32
DecaIi <bastien.porte@telecom-sudparis.eu>
2020-11-23 15:00:53 +0100

    juste befor la liste des taches

--- app/src/main/java/com/example/tp2androidstudio/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/example/tp2androidstudio/tasklist/TaskListFragment.kt
@@ -1,2 +1,21 @@
 package com.example.tp2androidstudio.tasklist
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tp2androidstudio.R
+
+class TaskListFragment : Fragment()
+{
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_task_list, container, false);
+        return rootView;
+    }
+
+}
