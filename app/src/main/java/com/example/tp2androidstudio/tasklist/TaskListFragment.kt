3d2e20cd97803be206d73e4b818d039bfbec6af9
DecaIi <bastien.porte@telecom-sudparis.eu>
2020-11-23 17:12:04 +0100

    end of the tp

--- app/src/main/java/com/example/tp2androidstudio/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/example/tp2androidstudio/tasklist/TaskListFragment.kt
@@ -8,14 +8,18 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tp2androidstudio.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.UUID.randomUUID
 
 class TaskListFragment : Fragment()
 {
-    private val taskList = listOf(
+    private val taskList = mutableListOf(
         Task(id = "id_1", title = "Task 1", description = "description 1"),
         Task(id = "id_2", title = "Task 2"),
         Task(id = "id_3", title = "Task 3")
     )
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -32,6 +36,8 @@ class TaskListFragment : Fragment()
         var recyclerView = view.findViewById<RecyclerView>(R.id.recyclerview);
         recyclerView.layoutManager= LinearLayoutManager(activity);
         recyclerView.adapter = TaskListAdapter(taskList);
+        val buton = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        buton.setOnClickListener {taskList.add(Task(id = randomUUID().toString(), title = "Task ${taskList.size + 1}"));(recyclerView.adapter as TaskListAdapter).notifyDataSetChanged() }
     }
 
 }
